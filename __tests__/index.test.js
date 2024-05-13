7bab8849abb48566a44c3f88404817425ceb9fa6
Дмитрий Кабацюра <cauliflb@student.21-school.ru>
2024-05-13 11:31:36 +0300

    UPD Step #5 is done

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,10 +10,10 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 
-describe('gendiff()', () => {
-  test('Check a difference between flat objects', () => {
+describe('gendiff()', () => { // eslint-disable-line
+  test('Check a difference between flat objects', () => { // eslint-disable-line
     const file1 = getFixturePath('file1.json');
     const file2 = getFixturePath('file2.json');
-    expect(genDiff(file1, file2)).toEqual(checkTest);
+    expect(genDiff(file1, file2)).toEqual(checkTest); // eslint-disable-line
   });
 });
--- src/parsers.js
+++ src/parsers.js
@@ -9,7 +9,7 @@ const parser = (data, format) => parse[format](data);
 
 // The process.cwd() method returns the current working directory of the Node.js process.
 // eslint-diable-next-line
-export const getPath = (filePath) => path.resolve(process.cwd(), filePath);
+export const getPath = (filePath) => path.resolve(process.cwd(), filePath); // eslint-disable-line
 
 // Нахожу расширение файла
 export const getExtension = (filePath) => path.basename(filePath).split('.').slice(1);
